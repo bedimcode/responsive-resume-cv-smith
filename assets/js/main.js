bcd0c4d506bf2b4ac51a11636e72fa51aa0e6e05
bedimcode <61248995+bedimcode@users.noreply.github.com>
2021-02-12 00:50:19 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+/*==================== SHOW MENU ====================*/
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*==================== SHOW SCROLL TOP ====================*/ 
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
+
+
+/*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/ 
+
+
+/*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/ 
+
+
+/*==================== GENERATE PDF ====================*/ 
+// PDF generated area
+
+
+// Html2pdf options
+
+
+// Function to call areaCv and Html2Pdf options 
+
+
+// When the button is clicked, it executes the three functions
+
+    // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
+
+
+    // 2. The PDF is generated
+
+
+    // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
